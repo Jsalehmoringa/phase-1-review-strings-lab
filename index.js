79dd7e7af16c6fae80c3f4caf5208079ca160112
jamil <jamil.saleh@student.moringaschool.com>
2022-11-26 07:41:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+var welcomeMessage = 'Welcome to Flatbook, ' 
+ welcomeMessage = `Welcome to Flatbook, ${currentUser}`
+ welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+var excitedWelcomeMessage = `WELCOME TO FLATBOOK,` ;
+const upperCaseCurrentUser = currentUser.toUpperCase();
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}`;
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !` ;
+    var shortGreeting = `Welcome, ${currentUser}`
+     shortGreeting = `Welcome, ${currentUser} !`
+     shortGreeting = `Welcome, ${currentUser}`
+     const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+shortGreeting = `Welcome, ${firstInitial}`;
+shortGreeting = `Welcome, ${firstInitial}!` 
+
